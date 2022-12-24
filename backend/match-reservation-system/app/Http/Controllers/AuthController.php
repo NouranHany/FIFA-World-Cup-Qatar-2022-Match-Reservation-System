f3ac5e78635ff79bfb6d15488486e544bc8520a9
NouranHany <hany.noran@yahoo.com>
2022-12-24 22:19:00 +0200

    Adding login, logout, signup routes

---
+++ backend/match-reservation-system/app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        // validate on the request body
+        $validated = $request->validate([
+            'email_address' => ['required'],
+            'password' => ['required']
+        ]);
+
+        // if the email and password aren't correct 
+        if (!auth()->attempt($validated)){
+            return response([
+                'message' => 'This user does not exist or password incorrect'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        
+        // create a token for this session
+        // respond with the user and its token
+        return response([
+            'user' => auth()->user(),
+            'access_token' => auth()->user()->createToken('authToken')->accessToken
+        ], Response::HTTP_OK);
+        
+    }
+
+    public function signup(Request $request)
+    {
+        // validate request
+        $validated = $request->validate([
+            'email_address' => ['required', 'email', 'unique:users,email_address'],
+            'password' => ['required', 'min:6'],
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'birth_date' => ['required'],
+            'gender' => ['required'],
+            'role' => ['required', Rule::in([0, 1, 2])] // should be only 0?
+        ]);
+        
+        $validated['password'] = Hash::make($request->password);
+        
+        // create user in database with the data in request body
+        $user = User::create($validated);
+
+        // create token
+        $token = $user->createToken('authToken')->accessToken;
+
+        // respond back with the token and created user
+        return response([
+            'user' => $user,
+            'token' => $token
+        ], Response::HTTP_OK);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response([
+            "message" => "you logged out successfully!"
+        ]);
+    }
+}
---
+++ backend/match-reservation-system/app/Models/User.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+// use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $primaryKey = 'username';
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'password',
+        'email_address',
+        'gender',
+        'nationality',
+        'role',
+        'requesting_promotion',
+        'birth_date'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
