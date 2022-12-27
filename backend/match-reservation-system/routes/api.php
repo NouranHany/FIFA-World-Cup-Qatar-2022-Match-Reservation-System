376ea44ec20c72c5c71baf9e531bdc6fdb90de6d
NouranHany <hany.noran@yahoo.com>
2022-12-27 21:32:47 +0200

    Adding postman collection

--- backend/match-reservation-system/routes/api.php
+++ backend/match-reservation-system/routes/api.php
@@ -22,24 +22,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-################## Authorization Routes ####################
+############################## Authorization Routes ####################
 Route::post('login', [AuthController::class, 'login']);
 Route::post('signup', [AuthController::class, 'signup']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
 ############################## Stadium Routes ################### 
-Route::post('/stadium', [StadiumController::class, 'store']);
+Route::post('/stadium', [StadiumController::class, 'store'])->middleware('auth:api');;
 
 
 ############################## Match Routes ################### 
-Route::post('/match', [MatcheController::class, 'store']);
-Route::put('/match/{match_id}', [MatcheController::class, 'update']);
+Route::post('/match', [MatcheController::class, 'store'])->middleware('auth:api');;
+Route::put('/match/{match_id}', [MatcheController::class, 'update'])->middleware('auth:api');;
 
-################## User Routes ####################
+############################## User Routes ####################
 // Route::get('user/{username}', [UserController::class, 'show']);
 Route::delete('user/{username}', [UserController::class, 'delete'])->middleware('auth:api');
-Route::post('user/approve/{username}', [UserController::class, 'approve']);
-Route::post('user/approve', [UserController::class, 'approve_all']);
-Route::get('user/unapproved-users', [UserController::class, 'unapproved_users']);
+Route::post('user/approve/{username}', [UserController::class, 'approve'])->middleware('auth:api');
+Route::post('user/approve', [UserController::class, 'approve_all'])->middleware('auth:api');
+Route::get('user/unapproved-users', [UserController::class, 'unapproved_users'])->middleware('auth:api');
 
 
